af44071a96a9721d373f8e8c2cacc8792e217311
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@gmail.com>
2020-07-31 20:38:18 -0300

    Implementado CRUD de Departamento e View da tela de departamento, por meio do scaffolnding.

---
+++ SistemaWebVendas/Data/SistemaWebVendasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaWebVendas.Models;
+
+namespace SistemaWebVendas.Data
+{
+    public class SistemaWebVendasContext : DbContext
+    {
+        public SistemaWebVendasContext (DbContextOptions<SistemaWebVendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaWebVendas.Models.Departamento> Departamento { get; set; }
+    }
+}
